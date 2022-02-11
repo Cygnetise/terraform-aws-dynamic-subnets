8a425f9df36a5662ee0b6f96407409a63db1a490
David Dawson <david.dawson@gmail.com>
2022-02-11 13:27:21 +0000

    remove nat-gateway and nat-instance modules

--- outputs.tf
+++ outputs.tf
@@ -13,16 +13,6 @@ output "private_route_table_ids" {
   value       = aws_route_table.private.*.id
 }
 
-output "nat_gateway_ids" {
-  description = "IDs of the NAT Gateways created"
-  value       = aws_nat_gateway.default.*.id
-}
-
-output "nat_instance_ids" {
-  description = "IDs of the NAT Instances created"
-  value       = aws_instance.nat_instance.*.id
-}
-
 output "availability_zones" {
   description = "List of Availability Zones where subnets were created"
   value       = var.availability_zones
