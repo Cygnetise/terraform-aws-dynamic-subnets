0b37c9713b82675f608590b79d4c424b76236667
FLemon <jinfengxie1986@gmail.com>
2025-04-15 09:19:29 +0100

    remove default inline acl rule

--- public.tf
+++ public.tf
@@ -79,24 +79,6 @@ resource "aws_network_acl" "public" {
   vpc_id     = var.vpc_id
   subnet_ids = aws_subnet.public.*.id
 
-  egress {
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
-    protocol   = "-1"
-  }
-
-  ingress {
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
-    protocol   = "-1"
-  }
-
   tags = module.public_label.tags
 }
 
